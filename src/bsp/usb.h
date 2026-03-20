953576230b6ed399e8a8fb24028528116e9175e7
PhucLuu410 <luuphuc20012001@gmail.com>
2026-03-20 18:28:59 +0700

    2nd

--- src/bsp/usb.h
+++
@@ -1,6 +0,0 @@
-#ifndef __USB_H
-#define __USB_H
-
-void USB_Init(void);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include <stm32f103xb.h>
 #include "app/app.h"
 #include "bsp/bsp.h"
-
 extern volatile uint32_t g_tick_ms;
 
 int main(void)
 {
     System_Init();
     USB_Init();
-
     while (1)
     {
     }
